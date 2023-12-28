3f453590d05d09821ced4cffc6de495af1e71a86
demouser <demouser@gmail.com>
2023-12-28 10:28:06 +0530

    first commit

---
+++ DDemo/DDemo/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from api import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", views.Sum),
+    path('cal/', views.Calculate),
+    path('mul/', views.Mul),
+]
---
+++ DDemo/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def Sum(request):
+    return HttpResponse("knki dihdkd h")
+
+def Calculate(request):
+    return HttpResponse("calculate")
+
+def Mul(request):
+    return HttpResponse("multiplay")
+    
