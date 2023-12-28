37135f2a22477d66e525f3cb8e9e479a6279d9e8
demouser <demouser@gmail.com>
2023-12-28 11:09:46 +0530

    change model Module add Some field

--- DDemo/api/models.py
+++ DDemo/api/models.py
@@ -6,3 +6,4 @@ class Module(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length = 12, null=True)
