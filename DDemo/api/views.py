f43149808c96b3646da9a0f20b8e2ece59ac82b2
demouser <demouser@gmail.com>
2024-01-02 15:06:34 +0530

    ----

--- DDemo/api/views.py
+++ DDemo/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Module
 # Create your views here.
 def Sum(request):
     return HttpResponse("knki dihdkd h")
@@ -12,3 +12,8 @@ def Mul(request):
     
 def asd(request):
     return HttpResponse("kkjhgg")
+
+def index(request):
+    Module.objects.create(first_name = "Arit", last_name="Roy", email="example@gmail.com", phone="12345")
+    return HttpResponse("kkjhgg")
+
